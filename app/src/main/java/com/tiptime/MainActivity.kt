332658414cc86fd76cb4454f3fb487ca61dc2448
Luis Fernando Paganini <luisfernando_paganini@hotmail.com>
2024-06-22 14:20:16 -0300

    feat: theme

--- app/src/main/java/com/tiptime/MainActivity.kt
+++ app/src/main/java/com/tiptime/MainActivity.kt
@@ -1,14 +1,57 @@
 package com.tiptime
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener{
+            btCalculateButtonOnClick()
+        }
+        if (savedInstanceState != null)  {
+            val tip = savedInstanceState.getString("tip")
+            binding.tipResult.text = tip
+        } else
+            binding.tipResult.text = getString(R.string.tip_amount_s, "-")
+
+    }
+
+    private fun btCalculateButtonOnClick() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull() ?: return
+
+        val selectedId = binding.tipOption.checkedRadioButtonId
+
+        val tipPercentage = when( selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            R.id.option_fifteen_percent -> 0.15
+            else -> 0.0
+        }
+
+        var tip = cost * tipPercentage
+
+        val roundUp = binding.roundTip.isChecked
+
+        if ( roundUp )
+            tip = kotlin.math.ceil(tip)
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount_s, formattedTip)
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("tip", binding.tipResult.text.toString())
     }
 }
