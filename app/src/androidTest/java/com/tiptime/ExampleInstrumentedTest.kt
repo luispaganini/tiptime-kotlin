5717c2d5dae1d2e5d2a3b64ddc57a6f90a3b72db
Luis Fernando Paganini <luisfernando_paganini@hotmail.com>
2024-06-22 14:49:43 -0300

    feat: instrumented test

--- app/src/androidTest/java/com/tiptime/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/tiptime/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package com.tiptime
 
+import android.graphics.ColorSpace
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +24,18 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
     @Test
     fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.tiptime", appContext.packageName)
+        Espresso.onView(ViewMatchers.withId(R.id.cost_of_service))
+            .perform(ViewActions.typeText("50.00"))
+            .perform(ViewActions.closeSoftKeyboard())
+
+        Espresso.onView(ViewMatchers.withId(R.id.calculate_button))
+            .perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.tip_result))
+            .check(ViewAssertions.matches(ViewMatchers.withText(CoreMatchers.containsString("10.00"))))
     }
 }
